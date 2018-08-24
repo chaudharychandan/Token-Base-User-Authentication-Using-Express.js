63eb86cbc4c184f76894701db110a8580ad66f80
Chandan Kumar <chandanchaudhary@outlook.com>
2018-08-25 00:25:21 +0530

    Create a simple express starter app

---
+++ index.js
@@ -0,0 +1,19 @@
+// Module imports
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+
+// App setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({
+  type: '*/*'
+}));
+router(app);
+
+// Server Setup
+const port = process.env.PORT || 3090;
+app.listen(port);
+console.log('Server listening to port 3090');
