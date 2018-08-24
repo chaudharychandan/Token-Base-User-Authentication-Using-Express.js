17048cff12818405f18c513922e7e99378a3e630
Chandan Kumar <chandanchaudhary@outlook.com>
2018-08-25 00:26:14 +0530

    Configure db connection using mongodb running in localhost

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose  = require('mongoose');
 const app = express();
 const router = require('./router');
+
+// DB Setup
+mongoose.connect('mongodb://localhost:27017/auth', { useNewUrlParser: true });
 
 // App setup
 app.use(morgan('combined'));
