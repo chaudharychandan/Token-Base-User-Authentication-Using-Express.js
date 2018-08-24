531fb9d751b7392e95c994237f0f62f7da2e35a7
Chandan Kumar <chandanchaudhary@outlook.com>
2018-08-25 01:18:43 +0530

    Add bcrypt to encrypt the password Encrypt the password in pre save hook of user

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // Define our model
 const userSchema = new Schema({
@@ -13,6 +14,25 @@ const userSchema = new Schema({
     required: true,
   }
 });
+
+// On save hook, encrypt password
+userSchema.pre('save', function(next) {
+  const { password } =  this;
+
+  // generate a salt
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    // hash our password using the salt
+    bcrypt.hash(password, salt, null, (err, hash) => {
+      if (err) return next(err);
+
+      // overrite plain text password with encrypted password
+      this.password = hash;
+      next();
+    });
+  });
+})
 
 // Create the model class
 const UserModel = mongoose.model('user', userSchema);
